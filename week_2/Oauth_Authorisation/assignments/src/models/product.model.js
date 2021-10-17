173a2ff53203f116ef694136814cf403bb683960
sheetalsindhu <sheetalsindhu99@gmail.com>
2021-10-17 14:35:18 +0530

    oauth and authorisationn

---
+++ week_2/Oauth_Authorisation/assignments/src/models/product.model.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("product", productSchema); // products
